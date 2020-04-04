b672ab7990c44207b332022c91708ac7ce4e4d2f
Md. Kawser Habib <kawser.habib.dev@gmail.com>
2020-04-04 22:21:44 +0600

    ct problem, @rp+$torm.pcap scapy automation

---
+++ @RP+$torm.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+import base64
+
+pkts = rdpcap("ARP+Storm.pcap")
+
+flag = ''
+
+for pkt in pkts:
+    flag = flag + chr((pkt[ARP].op))
+
+print(flag)
+
+print(base64.b64decode(flag))
